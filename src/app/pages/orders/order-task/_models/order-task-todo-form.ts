e5f6cc9b8c570c3df510ef23f7d18f7dc2466d35
mkramer0820 <mkramer0820@gmail.com>
2018-10-11 17:03:56 -0400

    broke task drop down

--- src/app/pages/orders/order-task/_models/order-task-todo-form.ts
+++ src/app/pages/orders/order-task/_models/order-task-todo-form.ts
@@ -2,19 +2,19 @@ import { FormControl, Validators } from '@angular/forms'
 import { OrderTaskTodo } from './order-task-todo';
 
 export class OrderTaskTodosForm {
-  todo = new FormControl()
-  duedate = new FormControl()
-  comment = new FormControl()
-  status = new FormControl()
+  todo = new FormControl();
+  duedate = new FormControl();
+  comment = new FormControl();
+  status = new FormControl();
   constructor(
     todo: OrderTaskTodo,
   ) {
     this.todo.setValue(todo.todo)
     this.todo.setValidators([Validators.required])
 
-    this.comment.setValue(todo.comment)
-    this.duedate.setValue(todo.duedate)
-    this.duedate.setValue(todo.duedate)
-    this.status.setValue(todo.duedate)
+    this.comment.setValue(todo.comment);
+    this.duedate.setValue(todo.duedate);
+    this.duedate.setValue(todo.duedate);
+    this.status.setValue(todo.duedate);
   }
 }
