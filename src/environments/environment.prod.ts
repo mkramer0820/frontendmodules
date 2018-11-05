600b6f26026e21204f2f4325186f30397551b55c
mkramer0820 <mkramer0820@gmail.com>
2018-11-05 16:33:07 -0500

    dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 import { transformAll } from "@angular/compiler/src/render3/r3_ast";
 
 export const environment = {
-  // production: transformAll,
-  //production: true,
-  //apiUrl: 'http://104.248.10.237/'
+  //production: transformAll,
   production: true,
-  apiUrl: 'http://127.0.0.1:8000/'
+  apiUrl: 'http://104.248.10.237/'
+  //production: true,
+  //apiUrl: 'http://127.0.0.1:8000/'
 };
